08f9778caf2e8e0720979eaf61c5ad8f6c9dbb8e
James Knighton <iamknighton@gmail.com>
2015-09-04 02:25:43 -0700

    a_or_an

---
+++ panoptes/ling/text/a_or_an.py
@@ -0,0 +1,41 @@
+import re
+
+
+class AOrAnClassifier(object):
+    """
+    Decide between "a" and "an".
+    """
+
+    def __init__(self):
+        # A/an-picking from the Linguistics ruby gem by Michael Granger:
+        # http://www.deveiate.org/projects/Linguistics/wiki/English
+        rules = [
+            # Exceptions (an honor).
+            ('euler|hour(?!i)|heir|honest|hono', 'an'),
+
+            # Abbreviations:
+            # Strings of capitals starting with a vowel-sound consonant followed
+            # by another consonant, and which are not likely to be real words.
+            (('(?!FJO|[HLMNS]Y.|RY[EO]|SQU|(F[LR]?|[HL]|MN?|N|RH?|'
+              'S[CHKLMNPTVW]?|X(YL)?)[AEIOU])[FHLMNRSX][A-Z]'), 'an'),
+
+            ('^[aefhilmnorsx][.-]', 'an'),
+            ('^[a-z][.-]', 'a'),
+
+            ('^[^aeiouy]', 'a'),
+            ('^e[uw]', 'a'),                 # eu like "you" (a European).
+            ('^onc?e', 'a'),                 # o like "wa" (a one-liner).
+            ('uni([^nmd]|mo)', 'a'),         # u like "you" (a university).
+            ('^u[bcfhjkqrst][aeiou]', 'a'),  # u like "you" (a uterus).
+            ('^[aeiou]', 'an'),              # Vowels (an owl).
+                                             # y like i (an yclept).
+            ('y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt)', 'an'),
+            ('', 'a')                        # Guess "a".
+        ]
+
+        self.rules = map(lambda (r, a): (re.compile(r), a), rules)
+
+    def classify(self, next_word):
+        for r, a in self.rules:
+            if r.search(next_word):
+                return a
