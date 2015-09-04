505154306c37a828de8fae06e815b1b25d7ee903
James Knighton <iamknighton@gmail.com>
2015-09-03 23:59:38 -0700

    Parse.dump()

--- panoptes/ling/parse/parse.py
+++ panoptes/ling/parse/parse.py
@@ -3,9 +3,9 @@ class Token(object):
     A single token in a parse.
     """
 
-    def __init__(self, index, word, tag, up, downs):
+    def __init__(self, index, text, tag, up, downs):
         self.index = index  # integer index
-        self.word = word    # word
+        self.text = text    # text
         self.tag = tag      # tag
         self.up = up        # (dep, Token or None)
         self.downs = downs  # list of (dep, Token)
@@ -19,3 +19,20 @@ class Parse(object):
     def __init__(self, tokens, root):
         self.tokens = tokens  # list of Tokens
         self.root = root      # the root Token in tokens
+
+    def dump(self):
+        print 'Parse {'
+
+        for t in self.tokens:
+            print '%d=%s/%s' % (t.index, t.text, t.tag),
+        print
+
+        def fix((rel, parent)):
+            if parent:
+                parent = parent.index
+            return ' '.join(map(str, [rel, parent]))
+
+        for t in self.tokens:
+            print fix(t.up), '->', t.index, '->', map(fix, t.downs)
+
+        print '}'
