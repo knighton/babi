8647c3ba62887ad9399730fe1d033779ec23529f
James Knighton <iamknighton@gmail.com>
2015-09-06 02:20:48 -0700

    Add count restriction handling to ShortcutManager.

--- panoptes/ling/tree/surface/util/shortcut.py
+++ panoptes/ling/tree/surface/util/shortcut.py
@@ -101,38 +101,66 @@ ALT        -         -           -
 
 class ShortcutManager(object):
     """
-    Saying and parsing of shortcuts.  Does not check that counts match.
-    That is the caller's respnosibility.
+    Saying and parsing of shortcuts.
     """
 
-    def __init__(self):
+    def __init__(self, count_restriction_checker):
+        # CountRestrictionChecker.
+        self.count_restriction_checker = count_restriction_checker
+
         # SurfaceCorrelative, ShortcutColumn -> tokens, is_archaic.
         self.cor_sh2ss_archaic = make_shortcut_table()
 
         # Tokens, is_archaic -> SurfaceCorrelative, ShortcutColumn.
         self.ss_archaic2cors_shs = v2kk_from_k2v(self.cor_sh2ss_archaic)
 
+        # thing -> shortcut columns.
+        C = ShortcutColumn
         self.thing2shortcut_cols = defaultdict(list, {
-            'person': [ShortcutColumn.ONE, ShortColumn.BODY],
-            'thing': [ShortcutColumn.THING],
-            'place': [ShortcutColumn.PLACE],
-            'source': [ShortcutColumn.SOURCE, ShortcutColumn.SOURCE_FROM],
-            'goal': [ShortcutColumn.GOAL],
-            'time': [ShortcutColumn.TIME],
-            'way': [ShortcutColumn.WAY, ShortcutColumn.WAY_BY],
-            'reason': [ShortcutColumn.REASON, ShortcutColumn.REASON_FORE,
-                       ShortcutColumn.REASON_LATIN],
+            'person': [C.ONE, C.BODY],
+            'thing': [C.THING],
+            'place': [C.PLACE],
+            'source': [C.SOURCE, C.SOURCE_FROM],
+            'goal': [C.GOAL],
+            'time': [C.TIME],
+            'way': [C.WAY, C.WAY_BY],
+            'reason': [C.REASON, C.REASON_FORE, C.REASON_LATIN],
         })
 
+        # shortcut column -> thing.
         self.shortcut_col2thing = v2k_from_k2vv(self.thing2shortcut_cols)
 
-    def say(self, prep, cor, thing, allow_archaic):
+        # Correlative -> count restriction, grammatical number override.
+        C = SurfaceCorrelative
+        R = CountRestriction
+        self.cor2counts = {
+            C.INDEF: (None, None),
+            C.DEF: (None, None),
+            C.INTR: (R.ALL_ONE, None),
+            C.PROX: (R.ALL_ONE, None),
+            C.DIST: (R.ALL_ONE, None),
+            C.EXIST: (R.ONE_OF_N, None),
+            C.ELECT_ANY: (R.ONE_OF_N, None),
+            C.ELECT_EVER: (R.ALL_ONE, None),
+            C.UNIV_EVERY: (R.ALL, N2.SING),
+            C.UNIV_ALL: (R.ALL, N2.SING),
+            C.NEG: (R.NONE, N2.SING),
+            C.ALT: (R.ONE_OF_N, None),
+        }
+
+    def say(self, prep, n, of_n, cor, thing, allow_archaic):
         """
-        preposition, correlative, thing, allow archaic -> tokens or None
+        args -> tokens or None
 
         See if the preposition and noun and can expressed using a 'shortcut'.
         """
         # TODO: require and swallow prepositions correctly.
+
+        restriction = self.cor2counts[cor][0]
+        if not restriction:
+            return None
+        if not self.count_restriction_checker.is_possible(restriction, n, of_n):
+            return None
 
         if allow_archaic:
             use_archaics = [False, True]
@@ -150,12 +178,13 @@ class ShortcutManager(object):
 
     def parse(self, ss):
         """
-        tokens -> list of (preposition, correlative, thing)
+        tokens -> list of (preposition, correlative, thing, gram num override)
         """
         rr = []
         for is_archaic in [False, True]:
             for cor, shortcut_col in self.ss_archaic2cors_shs[(ss, is_archaic)]:
                 thing = self.shortcut_col2thing[shortcut_col]
-                r = (None, cor, thing)
+                gram_num_override = self.cor2counts[cor][1]
+                r = (None, cor, thing, gram_num_override)
                 rr.append(r)
         return rr
