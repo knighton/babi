cbe9dc362e677adc9fab31de7a25152e182c8247
James Knighton <iamknighton@gmail.com>
2015-09-11 19:28:16 -0700

    Idiolect.

---
+++ panoptes/ling/glue/idiolect.py
@@ -0,0 +1,32 @@
+class Idiolect(object):
+    """
+    Collection of flags that decide amongst variations in how it can say things.
+    """
+
+    def __init__(self, archaic_shortcuts=False, contractions=True,
+                 pedantic_plurals=False, stranding=True, split_infinitive=True,
+                 subjunctive_were=True, whom=False):
+        # Whether to use 'archaic' shortcuts ("come hither" vs "come here").
+        self.archaic_shortcuts = archaic_shortcuts
+
+        # Whether to use contractions.
+        self.contractions = contractions
+
+        # Whether to use pedantic plurals ("genies" vs "genii").
+        self.pedantic_plurals = pedantic_plurals
+
+        # Whether to split verb infinitives ("to not go" vs "not to go")
+        self.split_infinitive = split_infinitive
+
+        # Whether to use the subjunctive ("If I were president" vs "If I was
+        # president").
+        self.subjunctive_were = subjunctive_were
+
+        # Whether to strand the preposition of a fronted argument ("What are you
+        # talking about?" vs "About what are you talking?").
+        #
+        #   https://en.wikipedia.org/wiki/Preposition_stranding
+        self.stranding = stranding
+
+        # Whether to use 'whom' (pedantic), or replace it with 'who' (casual).
+        self.whom = whom
