acef5c6d001a9618740b4208944f1532a09a22d3
James Knighton <iamknighton@gmail.com>
2015-09-13 16:21:07 -0700

    Add tests, expose a bug.

--- tests/test_relation.py
+++ tests/test_relation.py
@@ -3,13 +3,69 @@ from panoptes.ling.glue.relation import Relation, RelationArgType, \
 
 
 def main():
+    THING = RelationArgType.THING
+
     m = RelationManager()
 
+    # "I walked to the store".
     rels_rats = [
-        (Relation.AGENT, RelationArgType.THING),
-        (Relation.TARGET, RelationArgType.THING),
+        (Relation.AGENT, THING),
+        (Relation.TO_LOCATION, THING),
     ]
-    print m.decide_preps(rels_rats)
+    assert m.decide_preps(rels_rats) == [None, ('to',)]
+
+    # "I baked a cake".
+    rels_rats = [
+        (Relation.AGENT, THING),
+        (Relation.TARGET, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [None, None]
+
+    # "The cake was baked by you."
+    rels_rats = [
+        (Relation.TARGET, THING),
+        (Relation.AGENT, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [None, ('by',)]
+
+    # "The cake was baked for you."
+    rels_rats = [
+        (Relation.TARGET, THING),
+        (Relation.FOR_RECIPIENT, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [None, ('for',)]
+
+    # "I baked you a cake.
+    rels_rats = [
+        (Relation.AGENT, THING),
+        (Relation.FOR_RECIPIENT, THING),
+        (Relation.TARGET, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [None, None, None]
+
+    # "I baked a cake for you."
+    rels_rats = [
+        (Relation.AGENT, THING),
+        (Relation.TARGET, THING),
+        (Relation.FOR_RECIPIENT, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [None, None, ('for',)]
+
+    # "For you I baked a cake."
+    rels_rats = [
+        (Relation.FOR_RECIPIENT, THING),
+        (Relation.AGENT, THING),
+        (Relation.TARGET, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [('for',), None, None]
+
+    # "For you a cake was baked by me."
+    rels_rats = [
+        (Relation.FOR_RECIPIENT, THING),
+        (Relation.TARGET, THING),
+        (Relation.AGENT, THING),
+    ]
+    assert m.decide_preps(rels_rats) == [('for',), None, ('by',)]
 
 
 if __name__ == '__main__':
