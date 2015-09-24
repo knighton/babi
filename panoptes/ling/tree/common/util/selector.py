01369847c9a921b8cfb1f87a7ff753f62c3f7b04
James Knighton <iamknighton@gmail.com>
2015-09-23 21:39:39 -0700

    Selector -> Melange (see comment); CNGN -> Selector.

--- panoptes/ling/tree/common/util/selector.py
+++ panoptes/ling/tree/common/util/selector.py
@@ -1,12 +1,21 @@
 from panoptes.ling.glue.grammatical_number import N5, nx_le_nx_is_guaranteed
 
 
-# Selectors allow determiners, articles, quantifiers, and the like.
+# "Melange" covers articles, interrogatives, demonstratives, and quantifiers, as
+# in Zamenhoff's table of correlatives.
+#
+#   https://en.wikipedia.org/wiki/Pro-form
+#
+# On naming: it's kind of a mixture of different types of pro-forms.  I need
+# "Selector" to refer to the object that contains count information as well as
+# this enum.  Also,
+#
+#   https://en.wikipedia.org/wiki/Melange_(fictional_drug)
 #
 # Different types of elective and universal correlatives use different
 # determiners/pronouns and may have different grammatical numbers (eg, "all cats
-# are black" but "every cat is black").
-Selector = enum("""Selector =
+# are black" but "every cat is black"), so those entries are split.
+Melange = enum("""Melange =
     INDEF
     DEF
     INTR
@@ -22,12 +31,14 @@ Selector = enum("""Selector =
 """)
 
 
-class CommonNounGrammaticalNumber(object):
+class Selector(object):
     """
+    Internal field of common nouns.
+
     How common nouns handle grammatical number.
 
     Information is stored as:
-    * a selector (eg, all/some/one/unknown of them)
+    * a Quantifier (eg, all/some/one/unknown of them)
     * how many there are, as an N5 range (eg, singular/plural/two/many)
     * out of how many they were selected from, also an N5 range
 
@@ -40,9 +51,9 @@ class CommonNounGrammaticalNumber(object):
     * "no black cats" -> NEG zero of plural
     """
 
-    def __init__(self, selector, n_min, n_max, of_n_min, of_n_max):
-        self.selector = selector
-        assert Selector.is_valid(self.selector)
+    def __init__(self, melange, n_min, n_max, of_n_min, of_n_max):
+        self.melange = melange
+        assert Melange.is_valid(self.melange)
 
         self.n_min = n_min
         self.n_max = n_max
@@ -58,23 +69,25 @@ class CommonNounGrammaticalNumber(object):
 
     def dump(self):
         return {
-            'type': 'CommonNounGrammaticalNumber',
-            'selector': Selector.to_str[self.selector],
+            'type': 'Selector',
+            'melange': Melange.to_str[self.melange],
             'n_min': N5.to_str[self.n_min],
             'n_max': N5.to_str[self.n_max],
             'of_n_min': N5.to_str[self.n_min],
             'of_n_max': N5.to_str[self.n_min],
         }
 
+    def is_definite(self):
+        return self.melange == Melange.DEF
+
     def is_interrogative(self):
-        return self.selector == Selector.INTR
+        return self.melange == Melange.INTR
 
     @staticmethod
     def load(d, loader):
-        selector = Selector.from_str[d['selector']]
+        melange = Melange.from_str[d['melange']]
         n_min = N5.from_str[d['n_min']]
         n_max = N5.from_str[d['n_max']]
         of_n_min = N5.from_str[d['of_n_min']]
         of_n_max = N5.from_str[d['of_n_max']]
-        return CommonNounGrammaticalNumber(
-            selector, n_min, n_max, of_n_min, of_n_max)
+        return Selector(melange, n_min, n_max, of_n_min, of_n_max)
