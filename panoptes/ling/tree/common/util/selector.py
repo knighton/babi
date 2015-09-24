29cf40d9d6943ed6ab246979d49d72c012047645
James Knighton <iamknighton@gmail.com>
2015-09-24 00:18:20 -0700

    Melange -> Correlative, CorrelativeManager -> DetProManager

--- panoptes/ling/tree/common/util/selector.py
+++ panoptes/ling/tree/common/util/selector.py
@@ -2,26 +2,23 @@ from panoptes.ling.glue.grammatical_number import compints_from_nx, N2, N5, \
     nx_le_nx_is_guaranteed
 
 
-# "Melange" covers articles, interrogatives, demonstratives, and quantifiers, as
-# in Zamenhoff's table of correlatives.
+# "Correlative" covers articles, interrogatives, demonstratives, and
+# quantifiers, as in Zamenhoff's table of correlatives.
 #
 #   https://en.wikipedia.org/wiki/Pro-form
-#
-# On naming: it's kind of a mixture of different types of pro-forms.  I need
-# "Selector" to refer to the object that contains count information as well as
-# this enum.  Also,
-#
-#   https://en.wikipedia.org/wiki/Melange_(fictional_drug)
 #
 # Different types of elective and universal correlatives use different
 # determiners/pronouns and may have different grammatical numbers (eg, "all cats
 # are black" but "every cat is black"), so those entries are split.
-Melange = enum("""Melange =
+Correlative = enum("""Correlative =
     INDEF
     DEF
+
     INTR
+
     PROX
     DIST
+
     EXIST
     ELECT_ANY
     ELECT_EVER
@@ -74,9 +71,9 @@ class Selector(object):
     * "no black cats" -> NEG zero of plural
     """
 
-    def __init__(self, melange, n_min, n_max, of_n_min, of_n_max):
-        self.melange = melange
-        assert Melange.is_valid(self.melange)
+    def __init__(self, correlative, n_min, n_max, of_n_min, of_n_max):
+        self.correlative = correlative
+        assert Correlative.is_valid(self.correlative)
 
         self.n_min = n_min
         self.n_max = n_max
@@ -93,7 +90,7 @@ class Selector(object):
     def dump(self):
         return {
             'type': 'Selector',
-            'melange': Melange.to_str[self.melange],
+            'correlative': Correlative.to_str[self.correlative],
             'n_min': N5.to_str[self.n_min],
             'n_max': N5.to_str[self.n_max],
             'of_n_min': N5.to_str[self.n_min],
@@ -101,10 +98,10 @@ class Selector(object):
         }
 
     def is_definite(self):
-        return self.melange == Melange.DEF
+        return self.correlative == Correlative.DEF
 
     def is_interrogative(self):
-        return self.melange == Melange.INTR
+        return self.correlative == Correlative.INTR
 
     def to_compints(self):
         aa = []
@@ -119,9 +116,9 @@ class Selector(object):
 
         return aa, bb
 
-    def decide_n2(self, melange2res_gno):
+    def decide_n2(self, cor2res_gno):
         """
-        (Melange -> (CountRestriction or None, N2 or None)) -> N2 or None
+        (Correlative -> (CountRestriction or None, N2 or None)) -> N2 or None
 
         Get whether to say the owning common noun as singular or plural, taking
         exceptions into account.
@@ -129,7 +126,7 @@ class Selector(object):
         Returns None if we are not valid (requires the input table to
         fully determine).
         """
-        count_restriction, gram_num_override = melange2res_gno[self.melange]
+        count_restriction, gram_num_override = cor2res_gno[self.correlative]
 
         # If there is no CountRestriction, it's not possible (eg, DEF is okay
         # for correlatives ("the"), but there is no such shortcut).
@@ -161,9 +158,9 @@ class Selector(object):
 
     @staticmethod
     def load(d, loader):
-        melange = Melange.from_str[d['melange']]
+        correlative = Correlative.from_str[d['correlative']]
         n_min = N5.from_str[d['n_min']]
         n_max = N5.from_str[d['n_max']]
         of_n_min = N5.from_str[d['of_n_min']]
         of_n_max = N5.from_str[d['of_n_max']]
-        return Selector(melange, n_min, n_max, of_n_min, of_n_max)
+        return Selector(correlative, n_min, n_max, of_n_min, of_n_max)
