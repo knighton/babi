bbc2c319571289508dccde667cebc68765ad7f3c
James Knighton <iamknighton@gmail.com>
2015-09-25 19:14:16 -0700

    test_pro_adverb: someone in both directions.

--- tests/test_pro_adverb.py
+++ tests/test_pro_adverb.py
@@ -23,6 +23,21 @@ def main():
     assert selector.dump() == proximal_sing.dump()
     assert noun == 'place'
 
+    existential_sing = Selector(
+        Correlative.EXIST, N5.SING, N5.SING, N5.DUAL, N5.MANY)
+    r = m.say(None, existential_sing, 'person', False)
+    assert r.tokens == ['someone']
+    assert r.conjugation == Conjugation.S3
+    assert r.eat_prep == False
+
+    ss = 'someone',
+    aaa = m.parse(ss)
+    assert len(aaa) == 1
+    prep, selector, noun = aaa[0]
+    assert prep == None
+    assert selector.dump() == existential_sing.dump()
+    assert noun == 'person'
+
 
 if __name__ == '__main__':
     main()
