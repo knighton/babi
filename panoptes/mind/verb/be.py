97a4ddb82a88c07bc9cbc5fd115393a43c8d707a
James Knighton <iamknighton@gmail.com>
2015-10-01 17:39:31 -0700

    Add missing.

---
+++ panoptes/mind/verb/be.py
@@ -0,0 +1,60 @@
+from panoptes.ling.glue.purpose import Purpose
+from panoptes.ling.glue.relation import Relation
+from panoptes.mind.idea import Identity
+from panoptes.mind.verb.base import ClauseMeaning, Response
+
+
+class AgentPlaceQuestion(ClauseMeaning):
+    def __init__(self):
+        self.purpose = Purpose.WH_Q
+        self.lemmas = ['be']
+        self.signatures = [
+            [Relation.AGENT, Relation.PLACE],
+        ]
+
+    def handle(self, c, memory, (agent_xx, loc_xx)):
+        if len(agent_xx) != 1:
+            return None
+
+        if len(loc_xx) != 1:
+            return None
+
+        agent = memory.ideas[agent_xx[0]]
+        place = memory.ideas[loc_xx[0]]
+
+        if agent.identity == Identity.REQUESTED:
+            return None
+        elif place.identity == Identity.REQUESTED:
+            x = agent.location
+            if x is None:
+                return Response('dunno')
+            loc = memory.ideas[x]
+            return Response(loc.kind)
+        else:
+            assert False
+
+
+class AgentInQuestion(ClauseMeaning):
+    def __init__(self):
+        self.purpose = Purpose.TF_Q
+        self.lemmas = ['be']
+        self.signatures = [
+            [Relation.AGENT, Relation.IN],
+        ]
+
+    def handle(self, c, memory, (agent_xx, place_xx)):
+        print 'AgentInQuestion'
+        if len(agent_xx) != 1:
+            return None
+
+        if len(place_xx) != 1:
+            return None
+
+        agent = memory.ideas[agent_xx[0]]
+        place_x, = place_xx
+        if agent.location is None:
+            return Response('dunno')
+        elif agent.location == place_x:
+            return Response('yes')
+        else:
+            return Response('no')
