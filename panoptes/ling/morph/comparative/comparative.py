a9b61971fc15092a34037b95c02f2e6ca8b6f0be
James Knighton <iamknighton@gmail.com>
2015-10-04 19:27:23 -0700

    ComparativeManager: decode(), add test.

--- panoptes/ling/morph/comparative/comparative.py
+++ panoptes/ling/morph/comparative/comparative.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import re
 import yaml
 
+from panoptes.etc.dicts import v2k_from_k2v
 from panoptes.etc.enum import enum
 
 
@@ -29,7 +30,7 @@ class ComparativeManager(object):
         self.erable = set(erable)
         self.not_erable = set(not_erable)
 
-        self.degree_polarity2pre = {
+        self.degree_polarity2adverb = {
             (ComparativeDegree.BASE,  ComparativePolarity.POS): None,
             (ComparativeDegree.COMP,  ComparativePolarity.POS): 'more',
             (ComparativeDegree.SUPER, ComparativePolarity.POS): 'most',
@@ -37,6 +38,7 @@ class ComparativeManager(object):
             (ComparativeDegree.COMP,  ComparativePolarity.NEG): 'less',
             (ComparativeDegree.SUPER, ComparativePolarity.NEG): 'least',
         }
+        self.adverb2degree_polarity = v2k_from_k2v(self.degree_polarity2adverb)
 
         self.repeat_last_chr_re = re.compile('.*[^aeiou][aeiou][^aeiouyw]$')
 
@@ -142,11 +144,75 @@ class ComparativeManager(object):
             assert False
 
     def encode(self, degree, polarity, base):
+        """
+        (degree, polarity, base) -> (None, adjier) or (more, adjy)
+        """
         assert base
         if polarity == ComparativePolarity.POS and self.is_erable(base):
-            pre = None
-            derived = self.encode_er_est(degree, base)
+            adverb = None
+            word = self.encode_er_est(degree, base)
         else:
-            pre = self.degree_polarity2pre[(degree, polarity)]
-            derived = base
-        return pre, derived
+            adverb = self.degree_polarity2adverb[(degree, polarity)]
+            word = base
+        return adverb, word
+
+    def decode_er_est(self, word):
+        # Exceptions (they cover every possibility for the words they include).
+        degrees_bases = self.exception_s2degrees_bases.get(word)
+        if degrees_bases:
+            return degrees_bases
+
+        # Catch positive (base) forms with derived-looking endings (eg, modest,
+        # clever, etc).
+        if word in self.base_er_est:
+            return [(ComparativeDegree.BASE, word)]
+
+        # -er and -est derivations work and reverse the same way.
+        degrees_bases = []
+        for suffix, degree in [('er', ComparativeDegree.COMP),
+                               ('est', ComparativeDegree.SUPER)]:
+            if word.endswith(suffix):
+                base = word[:-len(suffix)]
+                if 2 <= len(base) and base[-2] == base[-1] and \
+                        self.repeat_last_chr(base[:-1]):
+                    degrees_bases.append((degree, base[:-1]))
+                else:
+                    degrees_bases.append((degree, base))
+                    degrees_bases.append((degree, base + 'e'))
+
+        # Not -er or -est, so it's the base form.
+        if not degrees_bases:
+            return [(ComparativeDegree.BASE, word)]
+
+        # -i back to -y.
+        #
+        # It is almost always y (exceptions seem to be all nationalities, eg
+        # Iraqi).
+        rr = []
+        for degree, base in degrees_bases:
+            if base[-1] != 'i':
+                pass
+            elif base[0].isupper():
+                pass
+            else:
+                base = base[:-1] + 'y'
+            rr.append((degree, base))
+        return rr
+
+    def decode(self, adverb, word):
+        """
+        (None, adjier) or (more, adjy) -> list of (degree, polarity, base)
+        """
+        r = self.adverb2degree_polarity.get(adverb)
+        if not r:
+            return []
+        adverb_degree, polarity = r
+
+        # No adverb, so degree is found in the derived form.
+        if adverb_degree == ComparativeDegree.BASE and \
+                polarity == ComparativePolarity.POS:
+            degrees_bases = self.decode_er_est(word)
+            return zip(lambda (deg, base): (deg, polarity, base), degrees_bases)
+
+        # Has an adverb, so the word is in its base form.
+        return [(adverb_degree, polarity, word)]
--- tests/test_comparative.py
+++ tests/test_comparative.py
@@ -15,9 +15,29 @@ def check(m, base, degree, want_derived):
     try:
         assert want_ss == got_ss
     except:
+        print 'encode() failed.'
         print 'want:', want_ss
         print 'got:', got_ss
         raise
+
+    ok = False
+    degs_pols_bases = m.decode(want_derived[0], want_derived[1])
+    for deg, pol, got_base in degs_pols_bases:
+        if deg == degree and pol == ComparativePolarity.POS and \
+                got_base == base:
+            ok = True
+            break
+
+    if not ok:
+        print 'decode() failed.'
+        print 'input:', want_derived
+        print 'want:', ComparativeDegree.to_str[degree], \
+                ComparativePolarity.to_str[ComparativePolarity.POS], base
+        print 'got:'
+        for deg, pol, got_base in degs_pols_bases:
+            print '-', ComparativeDegree.to_str[deg], \
+                    ComparativePolarity.to_str[pol], got_base
+        assert False
 
 
 def main():
