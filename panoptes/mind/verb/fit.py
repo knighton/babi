8e3a68ea852862c183123815704d51c376a10dc2
James Knighton <iamknighton@gmail.com>
2015-10-04 22:08:03 -0700

    Implement "does it fit inside?".

--- panoptes/mind/verb/fit.py
+++ panoptes/mind/verb/fit.py
@@ -9,6 +9,7 @@ class FitsInside(ClauseMeaning):
         self.lemmas = ['fit']
         self.signatures = [
             [Relation.AGENT, Relation.INSIDE],
+            [Relation.AGENT, Relation.IN],
         ]
 
     def handle(self, c, memory, (agent_xx, inside_xx)):
@@ -23,3 +24,39 @@ class FitsInside(ClauseMeaning):
         memory.graph.link(agent_x, 'smaller', inside_x)
 
         return Response()
+
+
+class DoesItFitInside(ClauseMeaning):
+    def __init__(self):
+        self.purpose = Purpose.TF_Q
+        self.lemmas = ['fit']
+        self.signatures = [
+            [Relation.AGENT, Relation.INSIDE],
+            [Relation.AGENT, Relation.IN],
+        ]
+
+    def handle(self, c, memory, (agent_xx, inside_xx)):
+        if len(agent_xx) != 1:
+            return None
+
+        if len(inside_xx) != 1:
+            return None
+
+        agent_x, = agent_xx
+        inside_x, = inside_xx
+        path = memory.graph.decide_path(agent_x, inside_x)
+        if path is None:
+            return Response('dunno')
+
+        if not path:
+            return Response('what a stupid question')
+
+        rels = set(path)
+        if len(rels) != 1:
+            return Response('unclear')
+
+        rel = list(rels)[0]
+        if rel == 'bigger':
+            return Response('yes')
+        else:
+            return Response('no')
--- panoptes/mind/verb/manager.py
+++ panoptes/mind/verb/manager.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from panoptes.mind.verb.be import AgentIsTarget, AgentTargetQuestion, \
     AgentPlaceBeforeQuestion, AgentPlaceQuestion, AgentIn, AgentInQuestion
 from panoptes.mind.verb.carry import Bring, CarryingWhatQuestion, Drop, PickUp
-from panoptes.mind.verb.fit import FitsInside
+from panoptes.mind.verb.fit import DoesItFitInside, FitsInside
 from panoptes.mind.verb.go import GoTo, GoToAfter, GoToAt, HowGoFromToQuestion
 from panoptes.mind.verb.give import Give, GiveQuestion, ReceiveQuestion
 
@@ -26,6 +26,7 @@ class VerbSemanticsManager(object):
             CarryingWhatQuestion(),
 
             FitsInside(),
+            DoesItFitInside(),
 
             GoTo(),
             GoToAfter(),
