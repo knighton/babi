a4720db2f4f2fec80386641d5c4f8243a0f8b0f4
James Knighton <iamknighton@gmail.com>
2015-10-10 11:53:26 -0700

    Hax.

--- panoptes/mind/know/cause_effect.py
+++ panoptes/mind/know/cause_effect.py
@@ -3,7 +3,7 @@ import yaml
 
 
 CAUSE2EFFECTS = {}
-fn = 'data/cause_and_effect.yaml'
+fn = 'panoptes/mind/know/cause_and_effect.yaml'
 dd = yaml.load(open(fn))
 for d in dd:
     cause = d['cause']
